3b10a72d119ec4d00e5a61ac1780b8f21cd560ea
ObitoRin <2209246794@qq.com>
2022-02-26 00:19:53 +0800

    feat: react redux 版本

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import { store } from './index';
 
-function App() {
+function add1() {
+  store.dispatch({ type: 'add', payload: 1 });
+}
+
+function add2() {
+  store.dispatch({ type: 'add', payload: 2 });
+}
+
+function addIfOdd() {
+  if (store.getState() % 2 === 1) {
+    store.dispatch({ type: 'add', payload: 1 });
+  }
+}
+
+function addAsync() {
+  setTimeout(() => {
+    store.dispatch({ type: 'add', payload: 1 });
+  }, 2000);
+}
+
+function App(props) {
+  const { value } = props;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <p>{value}</p>
+        <button onClick={add1}>+1</button>
+        <button onClick={add2}>+2</button>
+        <button onClick={addIfOdd}>如果是单数就+1</button>
+        <button onClick={addAsync}>两秒钟后+1</button>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,13 +3,39 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux';
+// react + redux
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const reducer = (state, action) => {
+  if (state === undefined) {
+    return 0;
+  }
+  switch (action.type) {
+    case 'add':
+      console.log('state', state);
+      return state + action.payload;
+    default:
+      return 0;
+  }
+};
+
+export const store = createStore(reducer);
+
+const render = (store) => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App value={store.getState()} />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+};
+
+render(store);
+
+// 监听 state 变化就重新 render
+store.subscribe(() => {
+  render(store);
+});
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
